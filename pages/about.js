758ce8bc7250f063f6a97cfedd781b9549b42b52
Martin Banks <anotherbanksy@gmail.com>
2018-08-22 21:48:13 +1000

    cleanup

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,7 @@
-// const about = () => (
-//   <p>my about page</p>
-// )
+
 import React from 'react'
 
 class about extends React.Component {
-  
   render () {
     return <p>new about page</p>
   }
