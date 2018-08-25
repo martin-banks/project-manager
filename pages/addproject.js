c5741fc3c07184d6c0c5de17ad15c02e9bd8b557
Martin Banks <anotherbanksy@gmail.com>
2018-08-25 22:49:40 +1000

    updates form  for adding project data

--- pages/addproject.js
+++ pages/addproject.js
@@ -10,6 +10,51 @@ class addproject extends React.Component {
         <label for="name">Add a project name</label>
         <input name="name" type="text" placehodler="projectname" />
 
+        <label for="description">add a project description</label>
+        <textarea name="description" id="" cols="30" rows="5" />
+
+        <label for="keywords">Keywords</label>
+        <ul class="keywords">
+          <li>
+            <label for="keywords">Keyword</label>
+            <input type="checkbox" id="" value="" name="tech" />
+          </li>
+        </ul>
+        
+        <label for="tech">Tech used</label>
+        <ul class="tech">
+          <li>
+            <label for="javascript">JS</label>
+            <input type="checkbox" id="" value="" name="javscript" />
+          </li>
+        </ul>
+
+
+        <label for="publicUrl">Add a public URL</label>
+        <input name="publicUrl" type="text" placehodler="Public URL" />
+
+        <label for="privateUrl">Add a private URL</label>
+        <input name="privateUrl" type="text" placehodler="Private URL" />
+
+        <label for="publicRepo">Add a public repo</label>
+        <input name="publicRepo" type="text" placehodler="Public repo" />
+
+        <label for="privateRepo">Add a private repo</label>
+        <input name="privateRepo" type="text" placehodler="private repo" />
+
+        <label for="livedate">Live date</label>
+        <input type="date"/>
+
+        <label for="personal-project">Personal project</label>
+        <input name="personal-project" type="checkbox" />
+
+        <label for="news-project">news project</label>
+        <input name="news-project" type="checkbox" />
+
+        <label for="challenge-project">challenge project</label>
+        <input name="challenge-project" type="checkbox" />
+
+
         <label for="submit">Submit project</label>
         <input name="submit" type="submit" />
       </form>
