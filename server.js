1042442e5f286fea013e72b4f6d4c0f7b7a4c49d
Martin Banks <anotherbanksy@gmail.com>
2018-08-27 01:00:52 +1000

    loads project info

--- server.js
+++ server.js
@@ -2,12 +2,28 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const next = require('next')
 const mongoose = require('mongoose')
+const fs = require('fs')
+const path = require('path')
 
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
 const port = 3000
+
+function readFile (file) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(
+      path.join(__dirname, file),
+      (err, data) => {
+        if (err) {
+          return reject(err)
+        }
+        return resolve(data)
+      }
+    )
+  })
+}
 
 require('dotenv').config({ path: 'database.env' })
 const db = process.env.DATABASE
@@ -29,19 +45,31 @@ app.prepare()
     server.use(bodyParser.json())
     server.use(bodyParser.urlencoded({ extended: true }))
 
-    server.use((req, res, next) => {
+    server.use(async (req, res, next) => {
       // console.log('middle ware working')
       res.locals.test = 'this is a test local'
+      try {
+        res.locals.logo = await readFile('assets/logo.svg')
+      } catch (err) {
+        console.log(err)
+      }
       next()
     })
 
     // TODO -> Set up a router middleware
     // server.use('/', routes)
 
-    server.get('/p/:id', (req, res) => {
+    server.get('/p/:id', async (req, res) => {
       const page = '/project'
       const queryParams = { id: req.params.id }
-      app.render(req, res, page, queryParams)
+      try {
+        const details = await Store.findById(req.params.id)
+        queryParams.details = details
+        app.render(req, res, page, queryParams)
+      } catch (err) {
+        console.error(err)
+        app.render(req, res, '/404')
+      }
     })
 
     server.get('/projects', async (req, res) => {
