135f6a2c598755bede68976496b586d3b4a6a81c
Martin Banks <anotherbanksy@gmail.com>
2018-08-27 01:02:11 +1000

    begin styling proejct list

--- pages/projects.js
+++ pages/projects.js
@@ -2,6 +2,29 @@ import React from 'react'
 import Link from 'next/link'
 import MainLayout from '../layouts/main'
 
+const card = project => (
+  <a
+    href={ `/p/${project._id}` }
+    key={ `project-${project._id}` }
+  >
+    <div className="card">
+      <h3>{ project.name }</h3>
+    </div>
+    <style jsx>{`
+      .card {
+        transition: all 300ms;
+        padding: 8px;
+        margin-bottom: 4px;
+        border: solid 1px rgba(255,255,255, 0.2);
+        border-radius: 4px;
+        background: rgba(0,0,0, 0);
+      }
+      .card:hover {
+        background: rgba(0,0,0, 0.4);
+      }
+    `}</style>
+  </a>
+)
 
 class projects extends React.Component {
   static async getInitialProps (context) {
@@ -12,9 +35,12 @@ class projects extends React.Component {
   render () {
     return <MainLayout>
       <h1>Projects</h1>
-      <pre>{ JSON.stringify(this.props.projects, 'utf-8', 2) }</pre>
+      <div className="wrapper__cards">
+        { this.props.projects.map(card) }
+      </div>
+      {/* <pre>{ JSON.stringify(this.props.projects, 'utf-8', 2) }</pre> */}
 
-      <style jsx>{``}</style>
+      <style jsx >{``}</style>
     </MainLayout>
   }
 }
