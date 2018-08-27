516ad1eea7d3379640e12b62f1044c7c01459608
Martin Banks <anotherbanksy@gmail.com>
2018-08-27 22:45:56 +1000

    renders markdown and new component to toggle text/input

--- pages/project.js
+++ pages/project.js
@@ -1,7 +1,32 @@
 import React from 'react'
 import { withRouter } from 'next/router'
 import MainLayout from '../layouts/main'
+import Markdown from 'react-markdown'
 // import fetch from 'isomorphic-fetch'
+
+class Par extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      edit: false,
+    }
+  }
+
+  handleClick () {
+    const s = this.state
+    s.edit = !this.state.edit
+    this.setState(s)
+  }
+
+  render (props) {
+    return (
+      <div>
+        <button onClick={ this.handleClick.bind(this) } >Edit</button>
+        { this.state.edit ? <input value={ this.props.desc } /> : <p>{ this.props.desc }</p>}
+      </div>
+    )
+  }
+}
 
 class Project extends React.Component {
   static getInitialProps (context) {
@@ -12,7 +37,10 @@ class Project extends React.Component {
   render () {
     return <MainLayout>
       <h1>{ this.props.router.query.details.name }</h1>
-      <p>{ this.props.router.query.details.description || '--' }</p>
+      <Markdown source={ this.props.router.query.details.description } />
+
+      {/* <p>{ this.props.router.query.details.description || '--' }</p> */}
+      <Par desc={ this.props.router.query.details.description }/>
 
       <pre>{ JSON.stringify(this.props.router.query.details, 'utf-8', 2) }</pre>
 
