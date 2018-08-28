66317d67779b14f9a4d9f374742891f9f18c4b36
Martin Banks <anotherbanksy@gmail.com>
2018-08-28 14:14:40 +1000

    moves toggle par to input inline in main component

--- pages/project.js
+++ pages/project.js
@@ -9,20 +9,35 @@ class Par extends React.Component {
     super(props)
     this.state = {
       edit: false,
+      value: null,
     }
   }
 
-  handleClick () {
+  componentDidMount () {
     const s = this.state
-    s.edit = !this.state.edit
-    this.setState(s)
+    s.value = this.props.desc
+    this.setState({ s })
   }
 
-  render (props) {
+  // handleClick () {
+  //   const s = this.state
+  //   s.edit = !this.state.edit
+  //   this.setState(s)
+  // }
+
+  handleChange (e) {
+    this.props.handleChange(e)
+  }
+
+
+  render () {
     return (
       <div>
-        <button onClick={ this.handleClick.bind(this) } >Edit</button>
-        { this.state.edit ? <input value={ this.props.desc } /> : <p>{ this.props.desc }</p>}
+        <button onClick={ this.props.onClick } >Edit</button>
+        { this.props.edit
+            ? <input value={ this.state.value } onChange={ this.handleChange.bind(this) } /> 
+            : <p>{ this.state.value }</p>
+        }
       </div>
     )
   }
@@ -34,13 +49,52 @@ class Project extends React.Component {
     console.log({ logo })
     return logo
   }
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      edit: false
+    }
+  }
+
+  handleToggleEdit () {
+    const s = this.state
+    s.edit = !this.state.edit
+    console.log(this.state.edit)
+    this.setState(s)
+  }
+
+  handleChange (e) {
+    console.log('change!!!')
+    const s = this.state
+    s.desc = e.target.value
+    this.setState({ s })
+  }
+
+  componentDidMount () {
+    const s = this.state
+    s.desc = this.props.router.query.details.description
+    this.setState({ s })
+  }
+
   render () {
     return <MainLayout>
       <h1>{ this.props.router.query.details.name }</h1>
       <Markdown source={ this.props.router.query.details.description } />
 
       {/* <p>{ this.props.router.query.details.description || '--' }</p> */}
-      <Par desc={ this.props.router.query.details.description }/>
+      {/* <Par
+        desc={ this.props.router.query.details.description }
+        edit={ this.state.edit }
+        onClick={ this.handleToggleEdit.bind(this) }
+        onChange={ this.handleChange.bind(this) }
+      /> */}
+
+      <button onClick={ this.handleToggleEdit.bind(this) } >Edit</button>
+      { this.state.edit
+          ? <input value={ this.state.desc } onChange={ this.handleChange.bind(this) } /> 
+          : <p>{ this.state.desc }</p>
+      }
 
       <pre>{ JSON.stringify(this.props.router.query.details, 'utf-8', 2) }</pre>
 
