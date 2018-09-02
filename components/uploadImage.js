3e7f8a2628914e72839af9c7961f241209bb719e
Martin Banks <anotherbanksy@gmail.com>
2018-09-02 15:35:20 +1000

    removes image size versioning

--- components/uploadImage.js
+++ components/uploadImage.js
@@ -1,6 +1,5 @@
 import React from 'react'
 
-// will need function prop to send image file data back to parent
 export default class UploadImage extends React.Component {
   constructor (props) {
     super(props)
@@ -13,38 +12,6 @@ export default class UploadImage extends React.Component {
       warning: null,
       maxSize: 5,
     }
-    this.thumbnail = React.createRef()
-    this.display = React.createRef()
-  }
-
-  makeThumbnail (url) {
-    const crops = [
-      {
-        type: 'display',
-        maxWidth: 1000,
-      },
-      {
-        type: 'thumbnail',
-        maxWidth: 300,
-      },
-    ]
-    crops.forEach(crop => {
-      const { type, maxWidth } = crop
-      const image = new Image()
-      image.src = url
-      image.onload = () => {
-        const canvas = this[type].current
-        const ctx = canvas.getContext('2d')
-        const maxHeight = image.height / image.width * maxWidth
-        canvas.width = maxWidth
-        canvas.height = maxHeight
-        ctx.clearRect(0, 0, maxWidth, maxHeight)
-        ctx.drawImage(image, 0, 0, maxWidth, maxHeight)
-        const url = canvas.toDataURL('image/jpeg', 0.6)
-        this.setState({ thumbnail: url })
-        this.props.onUpload({ url, type })
-      }
-    })
   }
 
   handleImageUpload (e) {
@@ -54,7 +21,7 @@ export default class UploadImage extends React.Component {
     const mb = 1000000
 
     if (size > this.state.mazSize * mb) {
-      this.setState({ message: `Image is too large, please reduce to under ${this.state.mazSize}Mb` })
+      this.setState({ message: `Image is too large, please reduce to under ${this.state.maxSize}Mb` })
       return
     }
     reader.onloadend = async () => {
@@ -63,17 +30,13 @@ export default class UploadImage extends React.Component {
       image.onload = e => {
         const { width, height } = e.path[0]
         if (width / height < 1.7 || width / height > 1.8) {
-          this.setState({ warning: '⚠️ Images should be at a 16:9 ratio to prevent dynamic cropping' })
+          this.setState({ warning: '⚠️ Optimal image ratio is 16:9' })
         } else {
           this.setState({ warning: null })
         }
         this.setState({ 
           previewUrl: reader.result,
           file: file,
-        })
-        this.props.onUpload({
-          url: reader.result,
-          type: 'imageDisplay',
         })
         this.makeThumbnail(reader.result)
         return
@@ -85,28 +48,23 @@ export default class UploadImage extends React.Component {
   render () {
     return (
       <div className="wrapper">
-        <form>
-          <label htmlFor="file">Upload a display image</label>
-          <p><i>Images must be less than { this.state.maxSize }Mb</i></p>
-          <input
-            name="file"
-            id="file"
-            type="file"
-            accept="image/jpeg, image/png"
-            onChange={ this.handleImageUpload.bind(this) }
-          />
-        </form>
+        <label htmlFor="file">Upload a display image</label>
+        <p><i>Images must be less than { this.state.maxSize }Mb</i></p>
+        <input
+          name="file"
+          id="file"
+          type="file"
+          accept="image/jpeg, image/png"
+          onChange={ this.handleImageUpload.bind(this) }
+        />
 
         { this.state.warning ? <p className="warning">{ this.state.warning }</p> : '' }
 
-        {/* { 
+        { 
           this.state.previewUrl 
             ? <img src={ this.state.previewUrl } /> 
             : <p><i>{ this.state.message }</i></p>
-        } */}
-
-        <canvas ref={ this.display } width="0" height="0"></canvas>
-        <canvas ref={ this.thumbnail } width="0" height="0"></canvas>
+        }
 
         <style jsx>{`
           .wrapper {
@@ -123,13 +81,13 @@ export default class UploadImage extends React.Component {
             padding: 16px 0;
           }
           p.warning {
-            // color: #333;
             background: rgba(200, 150, 0, 0.2);
             padding: 12px;
             margin-bottom: 16px;
             border-radius: 4px;
           }
         `}</style>
+
       </div>
     )
   }
