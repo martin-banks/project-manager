dff64c5d11c9341486ab65962d1b57ce3185d185
Martin Banks <anotherbanksy@gmail.com>
2018-09-02 15:35:56 +1000

    adds sort results

--- functions/keywords.js
+++ functions/keywords.js
@@ -3,6 +3,7 @@ const ke = require('keyword-extractor')
 // https://www.npmjs.com/package/keyword-extractor
 
 function keywordExtractor (string) {
+  // console.log(string)
   const testString = 'This is my lunchtime string that I am writing at lunchtime on Thursday'
   // returns [ 'lunchtime', 'string', 'writing', 'thursday' ]
 
@@ -13,7 +14,13 @@ function keywordExtractor (string) {
     remove_duplicates: true,
   }
   const extraction = ke.extract(string, config)
-  
+    .sort((a, b) => {
+      // Sort the words alphabetically before returning them
+      if (a > b) return 1
+      if (a < b) return -1
+      return 0
+    })
+
   console.log(extraction)
   return extraction
 }
