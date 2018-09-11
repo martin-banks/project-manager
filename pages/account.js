e4d54091376bb161f2cd888407813b5e5524c048
Martin Banks <anotherbanksy@gmail.com>
2018-09-11 22:25:05 +1000

    adds details to form to allow user to update

--- pages/account.js
+++ pages/account.js
@@ -3,11 +3,30 @@ import { withRouter } from 'next/router'
 import Layout from '../layouts/main'
 
 class Account extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      
+    }
+  }
   static async getInitialProps (context) {
     return {
       locals: context.res.locals,
       url: context.url,
     }
+  }
+
+  componentDidMount () {
+    this.setState({
+      name: this.props.locals.user.name,
+      email: this.props.locals.user.email,
+    })
+  }
+
+  handleChange (e) {
+    const { value, name } = e.target
+    console.log({ name, value })
+    this.setState({ [name]: value })
   }
 
   render () {
@@ -17,6 +36,24 @@ class Account extends React.Component {
         url={ this.props.url }
       >
         <h1>Welcome back { this.props.locals.user.name }</h1>
+        <form action="account" method="POST">
+          <label htmlFor="name">Update you username</label>
+          <input
+            type="text"
+            name="name"
+            value={ this.state.name }
+            onChange={ this.handleChange.bind(this) }
+          />
+          <label htmlFor="email">Update you email</label>
+          <input
+            type="email"
+            name="email"
+            value={ this.state.email }
+            onChange={ this.handleChange.bind(this) }
+          />
+
+          <input type="submit" />
+        </form>
       </Layout>
     )
   }
