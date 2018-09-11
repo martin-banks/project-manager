17fd4f48abc031b8c128c54fe80552e09eb6ecd1
Martin Banks <anotherbanksy@gmail.com>
2018-09-11 22:51:26 +1000

    cleanup

--- pages/project.js
+++ pages/project.js
@@ -6,47 +6,6 @@ import {Image, Video, Transformation, CloudinaryContext} from 'cloudinary-react'
 // import fetch from 'isomorphic-fetch'
 
 
-// ! DEPRICATED
-// class Par extends React.Component {
-//   constructor (props) {
-//     super(props)
-//     this.state = {
-//       edit: false,
-//       value: null,
-//     }
-//   }
-
-//   componentDidMount () {
-//     const s = this.state
-//     s.value = this.props.desc
-//     this.setState({ s })
-//   }
-
-//   // handleClick () {
-//   //   const s = this.state
-//   //   s.edit = !this.state.edit
-//   //   this.setState(s)
-//   // }
-
-//   handleChange (e) {
-//     this.props.handleChange(e)
-//   }
-
-
-//   render () {
-//     return (
-//       <div>
-//         <button onClick={ this.props.onClick } >Edit</button>
-//         { this.props.edit
-//             ? <input value={ this.state.value } onChange={ this.handleChange.bind(this) } /> 
-//             : <p>{ this.state.value }</p>
-//         }
-//       </div>
-//     )
-//   }
-// }
-
-
 class Project extends React.Component {
   static async getInitialProps (context) {
     const output = {
@@ -61,29 +20,9 @@ class Project extends React.Component {
     this.state = {
       edit: false,
       details: {},
-      displayWidth: 0,
+      displayWidth: 1200,
     }
   }
-
-  // handleToggleEdit () {
-  //   const s = this.state
-  //   s.edit = !this.state.edit
-  //   console.log(this.state.edit)
-  //   this.setState(s)
-  // }
-
-  // handleChange (e) {
-  //   console.log('change!!!')
-  //   const s = this.state
-  //   s.desc = e.target.value
-  //   this.setState({ s })
-  // }
-
-  // componentDidMount () {
-  //   const s = this.state
-  //   s.desc = this.props.router.query.details.description
-  //   this.setState({ s })
-  // }
 
   componentDidMount() {
     this.setState({
@@ -111,28 +50,6 @@ class Project extends React.Component {
       pathname={ this.props.url.pathname }
       locals={ this.props.locals }
     >
-      {/* <Markdown source={ this.props.router.query.details.description } /> */}
-
-      {/* <p>{ this.props.router.query.details.description || '--' }</p> */}
-      {/* <Par
-        desc={ this.props.router.query.details.description }
-        edit={ this.state.edit }
-        onClick={ this.handleToggleEdit.bind(this) }
-        onChange={ this.handleChange.bind(this) }
-      /> */}
-
-
-      {/* 
-        // TODO consider separate layout 
-        // TODO loop over all content items 
-        // TODO submit changes back to server for DB update 
-      */}
-      {/* <button onClick={ this.handleToggleEdit.bind(this) } >Edit</button>
-      { this.state.edit
-          ? <input value={ this.state.desc } onChange={ this.handleChange.bind(this) } /> 
-          : <p>{ this.state.desc }</p>
-      } */}
-
       {/*
        // TODO Store content in state before component mount then itterate to render
       */}
@@ -144,7 +61,7 @@ class Project extends React.Component {
           width={ this.state.displayWidth }
           crop="scale"
         />
-        
+
         { name && <h1>{ name }</h1>}
 
         <h3>What</h3>
