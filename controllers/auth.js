843d08f068f4ad2e84a768459e7275c686267e5c
Martin Banks <anotherbanksy@gmail.com>
2018-09-11 22:24:25 +1000

    adds middleware to check user is authenticated

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,3 +6,16 @@ exports.login = passport.authenticate('local', {
   successRedirect: '/',
   successFlash: 'You are logged in',
 })
+
+exports.checkIfLoggedIn = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    next()
+    return
+  }
+  console.log('unlogged in user tried to access /addproject')
+  req.flash('error', 'You must be logged in to do that')
+  req.session.save(err => {
+    console.log('session saved ...')
+    res.redirect('/login')
+  })
+}
