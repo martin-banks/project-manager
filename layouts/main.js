a1b30f685600ecb2dec9873113c85897a83d703c
Martin Banks <anotherbanksy@gmail.com>
2018-09-12 01:19:00 +1000

    renders flash component

--- layouts/main.js
+++ layouts/main.js
@@ -1,11 +1,15 @@
 import React from 'react'
-import Nav from '../components/nav'
 import styles from '../styles'
+import Nav from '../components/nav'
+import Flashes from '../components/flashes'
 
 
 class MainLayout extends React.Component {
   constructor (props) {
     super(props)
+    this.state = {
+      showFlashes: true
+    }
   }
 
   componentDidMount () {
@@ -16,13 +20,27 @@ class MainLayout extends React.Component {
     //     this.setState({ showInput: true })
     //   }
     // })
+    setTimeout(() => {
+      this.setState({ showFlashes: false })
+    }, 3 * 1000)
   }
   render () {
+    const { flashes } = this.props.locals
     return <div>
       <Nav
         pathname={ this.props.pathname }
         user={ this.props.locals.user }
       />
+
+      { 
+        this.state.showFlashes && Object.keys(flashes)
+          .map((f, i) => <Flashes
+            type={ f }
+            message={ flashes[f] }
+            key={ `flash-${i}` } 
+          />)
+      }
+
       <div className="background__gradient"></div>
 
       <pre>
