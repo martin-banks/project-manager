ac01a527bf7fc415f611f98bf774796427a0d252
Martin Banks <anotherbanksy@gmail.com>
2018-09-15 19:52:14 +1000

    fixes classNAme assignment

--- components/nav.js
+++ components/nav.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 const Logo = () => (
   <svg
-    class="nav__logo"
+    className="nav__logo"
     width="20"
     height="20"
     xmlns="http://www.w3.org/2000/svg"
