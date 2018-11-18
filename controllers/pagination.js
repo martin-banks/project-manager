2fd99e1dffcf63c19c30d947b7ed0ef48b1f6d03
Martin Banks <anotherbanksy@gmail.com>
2018-11-18 23:21:12 +1100

    begins adding user check for project filtering

--- controllers/pagination.js
+++ controllers/pagination.js
@@ -1,9 +1,19 @@
 const mongoose = require('mongoose')
+const passport = require('passport')
+
+require('../models/User')
+const User = mongoose.model('User')
 
 require('../models/Project')
 const Project = mongoose.model('Project')
 
 exports.pagination = async (req, res, next) => {
+  // Check for a user and see if that user has private access
+  // const foundUser = await User.findOne({ loginToken: id })
+  // console.log({ foundUser })
+
+  console.log('user', req.user)
+
   const { page } = req.params
   const limit = 4
   let skip = (page * limit) - limit
@@ -15,7 +25,8 @@ exports.pagination = async (req, res, next) => {
       .sort({ created: -1 })
     const totalProjects = Project.count()
 
-    const [ projects, count ] = await Promise.all([ projectsPromise, totalProjects ])
+    const [ projects, count ] = await Promise
+      .all([ projectsPromise, totalProjects ])
     const pages = Math.ceil(count / limit)
 
     res.locals.pagination = { pages, page, limit }
@@ -24,6 +35,5 @@ exports.pagination = async (req, res, next) => {
     req.flash('error', err.toString())
     res.redirect('/')
   }
-    next()
+  next()
 }
-
