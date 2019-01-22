97f349e7eaabef5ef74df9be050b3960e98c3fd2
Martin Banks <anotherbanksy@gmail.com>
2019-01-22 14:10:38 +1100

    restyle cards

--- components/project-entry.js
+++ components/project-entry.js
@@ -6,48 +6,69 @@ export default class ProjectEntry extends React.Component {
     super(props)
   }
 
+  displayDate (d) {
+    const months = [
+      'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec',
+    ]
+    const newDate = new Date(d)
+    const year = newDate.getFullYear()
+    const month = months[newDate.getMonth()]
+    const days = newDate.getDate()
+    return `${days} / ${month} / ${year}`
+
+  }
+
   render () {
-    // console.log('project info', this.props.project)
+    console.log('project info', this.props.project)
     return <a
       className="card"
       href={ `/p/${this.props.project._id}` }
       key={ `project-${this.props.project._id}` }
     >
-      <div>
-        <div className="wrapper__image">
+      {/* <div> */}
+        {/* <div className="wrapper__image"> */}
+          <div className="wrapper__text">
+            <p>{ this.displayDate(this.props.project.liveDate) }</p>
+            <h3>{ this.props.project.name }</h3>
+          </div>
           <Image
             className="cloudinaryImage"
             cloudName="martinbanks"
             publicId={ this.props.project.display }
-            width="300"
+            width={ 600 }
             crop="scale"
           />
-        </div>
-        <div className="wrapper__text">
-          <h3>{ this.props.project.name }</h3>
-        </div>
-      </div>
+        {/* </div> */}
+      {/* </div> */}
       <style jsx>{`
-        {/* .card {
+        .card {
+          {/* outline: solid 1px purple; */}
           transition: all 300ms;
           flex: 1 1 auto;
           border-radius: 4px;
           background: rgba(0,0,0, 0);
-          min-width: 200px;
-          width: 300px;
-          max-width: 400px;
+          {/* min-width: 200px; */}
+          width: 100%;
+          max-width: 1000px;
           padding: 16px;
+          margin: 0 auto;
           margin-bottom: 8px;
+          display: flex;
+          align-items: center;
         }
         .card:hover {
-          background: rgba(0,0,0, 0.4);
+          background: rgba(50,0,50, 0.2);
+        }
+        .cloudinaryImage {
+          flex: 1 1 auto;
         }
         .wrapper__image {
           width: 100%;
           margin-bottom: 16px;
         }
         .wrapper__text {
-        } */}
+          flex: 1 1 auto;
+        }
       `}</style>
     </a>
   }
